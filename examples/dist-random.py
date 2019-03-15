f47880a9bb13dfed2e8faf5f4a28ae4b40c1c891
Francesc Alted <faltet@gmail.com>
2019-03-15 13:27:32 +0100

    Rename random.py so as to not collide with the Python module name

---
+++ examples/dist-random.py
@@ -0,0 +1,20 @@
+import iarray as ia
+import numpy as np
+import matplotlib.pyplot as plt
+
+cfg = ia.Config()
+ctx = ia.Context(cfg)
+r_ctx = ia.RandomContext(ctx)
+
+size = 1000000
+shape = [size]
+pshape = [100]
+
+a1 = ia.random_beta(ctx, r_ctx, 2, 5, shape, pshape)
+a2 = ia.iarray2numpy(ctx, a1)
+
+b = np.random.beta(2, 5, size)
+
+plt.hist([a2, b], bins='auto', density=True, histtype="step", cumulative=False)
+
+plt.show()
