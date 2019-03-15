786f0cff6b6e9443a0e31da2914c59334fc770a6
Francesc Alted <faltet@gmail.com>
2019-03-15 17:37:39 +0100

    Add the comparison against pre-compiled numba code

--- examples/compare-evaluators.py
+++ examples/compare-evaluators.py
@@ -1,20 +1,42 @@
+import iarray as ia
 from time import time
 import numpy as np
 import numexpr as ne
 from numba import jit
+from numba.pycc import CC
+import numba_prec  # for pre-compiled numba code
 
-import iarray as ia
 
+cc = CC('numba_prec')
+# Uncomment the following line to print out the compilation steps
+cc.verbose = True
 
-N = 1000 * 1000 * 10
+# Vector sizes and partitions
+N = 2 * 1000 * 1000
 shape = [N]
 pshape = [2 * 100 * 1000]
 block_size = pshape
 expression = '(x - 1.35) * (x - 4.45) * (x - 8.5)'
 
 
-@jit(nopython=True)
-def poly(x):
+def poly_python(x):
+    y = np.empty(x.shape, x.dtype)
+    for i in range(len(x)):
+        y[i] = (x[i] - 1.35) * (x[i] - 4.45) * (x[i] - 8.5)
+    return y
+
+
+# @jit(nopython=True, cache=True, parallel=True)
+@jit(nopython=True, cache=True)
+def poly_numba(x):
+    y = np.empty(x.shape, x.dtype)
+    for i in range(len(x)):
+        y[i] = (x[i] - 1.35) * (x[i] - 4.45) * (x[i] - 8.5)
+    return y
+
+
+@cc.export('poly_double', 'f8[:](f8[:])')
+def poly_numba_prec(x):
     y = np.empty(x.shape, x.dtype)
     for i in range(len(x)):
         y[i] = (x[i] - 1.35) * (x[i] - 4.45) * (x[i] - 8.5)
@@ -31,20 +53,29 @@ def do_regular_evaluation():
     # print(y0, y0.shape)
 
     t0 = time()
+    y1 = poly_python(x)
+    print("Regular evaluate via python:", round(time() - t0, 3))
+    np.testing.assert_almost_equal(y0, y1)
+
+    t0 = time()
     y1 = (x - 1.35) * (x - 4.45) * (x - 8.5)
     print("Regular evaluate via numpy:", round(time() - t0, 3))
-
-    t0 = time()
-    y2 = ne.evaluate(expression, local_dict={'x': x})
-    print("Regular evaluate via numexpr:", round(time() - t0, 3))
-
-    t0 = time()
-    y3 = poly(x)
-    print("Regular evaluate via numba:", round(time() - t0, 3))
-
     np.testing.assert_almost_equal(y0, y1)
-    np.testing.assert_almost_equal(y0, y2)
-    np.testing.assert_almost_equal(y0, y3)
+
+    t0 = time()
+    y1 = ne.evaluate(expression, local_dict={'x': x})
+    print("Regular evaluate via numexpr:", round(time() - t0, 3))
+    np.testing.assert_almost_equal(y0, y1)
+
+    t0 = time()
+    y1 = poly_numba(x)
+    print("Regular evaluate via numba:", round(time() - t0, 3))
+    np.testing.assert_almost_equal(y0, y1)
+
+    t0 = time()
+    y1 = numba_prec.poly_double(x)
+    print("Regular evaluate via pre-compiled numba:", round(time() - t0, 3))
+    np.testing.assert_almost_equal(y0, y1)
 
 
 def do_block_evaluation():
@@ -63,27 +94,35 @@ def do_block_evaluation():
         y[:] = (x - 1.35) * (x - 4.45) * (x - 8.5)
     print("Block evaluate via numpy:", round(time() - t0, 3))
     y1 = ia.iarray2numpy(ctx, ya)
+    np.testing.assert_almost_equal(y0, y1)
 
     t0 = time()
     ya = ia.empty(ctx, shape=shape, pshape=pshape)
     for ((i, x), (j, y)) in zip(xa.iter_block(block_size), ya.iter_write()):
         y[:] = ne.evaluate(expression, local_dict={'x': x})
     print("Block evaluate via numexpr:", round(time() - t0, 3))
-    y2 = ia.iarray2numpy(ctx, ya)
+    y1 = ia.iarray2numpy(ctx, ya)
+    np.testing.assert_almost_equal(y0, y1)
 
     t0 = time()
     ya = ia.empty(ctx, shape=shape, pshape=pshape)
     for ((i, x), (j, y)) in zip(xa.iter_block(block_size), ya.iter_write()):
-        y[:] = poly(x)
+        y[:] = poly_numba(x)
     print("Block evaluate via numba:", round(time() - t0, 3))
-    y3 = ia.iarray2numpy(ctx, ya)
-
+    y1 = ia.iarray2numpy(ctx, ya)
     np.testing.assert_almost_equal(y0, y1)
-    np.testing.assert_almost_equal(y0, y2)
-    np.testing.assert_almost_equal(y0, y3)
+
+    t0 = time()
+    ya = ia.empty(ctx, shape=shape, pshape=pshape)
+    for ((i, x), (j, y)) in zip(xa.iter_block(block_size), ya.iter_write()):
+        y[:] = numba_prec.poly_double(x)
+    print("Block evaluate via pre-compiled numba:", round(time() - t0, 3))
+    y1 = ia.iarray2numpy(ctx, ya)
+    np.testing.assert_almost_equal(y0, y1)
 
 
 if __name__ == "__main__":
+    cc.compile()
     cfg = ia.Config()
     ctx = ia.Context(cfg)
     do_regular_evaluation()
