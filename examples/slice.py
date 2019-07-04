deac5e00b98c75c145ea50faf2ca95dbc05b60f9
Francesc Alted <faltet@gmail.com>
2019-07-04 14:42:36 +0200

    Fixed remaining example for the new API

--- examples/slice.py
+++ examples/slice.py
@@ -1,31 +1,25 @@
 import iarray as ia
 import numpy as np
 
-cfg = ia.Config()
-ctx = ia.Context(cfg)
 
-ashape, apshape, astart, astop, abshape, bshape, bpshape, bstart, bstop, bbshape, dtype = ([100, 100], [20, 20], [20, 40], [70, 90], [23, 32],
-                              [100, 100], [30, 30], [10, 20], [60, 70], [32, 23], "double")
+ashape, apshape, astart, astop = [100, 100], [20, 20], [20, 40], [70, 90]
+bshape, bpshape, bstart, bstop = [100, 100], [30, 30], [10, 20], [60, 70]
+dtype = np.float64
 
 asize = int(np.prod(ashape))
-a = ia.linspace(ctx, asize, -10, 10, ashape, apshape, dtype)
-an = ia.iarray2numpy(ctx, a)
+a = ia.linspace(ia.dtshape(ashape, apshape, dtype), -10, 10)
+an = ia.iarray2numpy(a)
 aslices = tuple(slice(astart[i], astop[i]) for i in range(len(astart)))
 if len(astart) == 1:
     aslices = aslices[0]
 asl = a[aslices]
+print(ia.iarray2numpy(asl))
 
 bsize = int(np.prod(bshape))
-b = ia.linspace(ctx, bsize, -10, 10, bshape, bpshape, dtype)
-bn = ia.iarray2numpy(ctx, b)
+b = ia.linspace(ia.dtshape(bshape, bpshape, dtype), -10, 10)
+bn = ia.iarray2numpy(b)
 bslices = tuple(slice(bstart[i], bstop[i]) for i in range(len(bstart)))
 if len(bstart) == 1:
     bslices = bslices[0]
 bsl = b[bslices]
-
-c = ia.matmul(ctx, asl, bsl, abshape, bbshape)
-cn = np.matmul(an[aslices], bn[bslices])
-
-cn_2 = ia.iarray2numpy(ctx, c)
-
-np.testing.assert_almost_equal(cn, cn_2)
+print(ia.iarray2numpy(bsl))
