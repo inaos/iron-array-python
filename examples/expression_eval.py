796ea134d683dac071f62ddda02a4eaba2394a0d
Francesc Alted <faltet@gmail.com>
2019-12-16 14:31:37 +0100

    Improved example

--- examples/expression_eval.py
+++ examples/expression_eval.py
@@ -1,30 +1,40 @@
 import iarray as ia
 import numpy as np
 
+# Number of iterations per benchmark
+NITER = 10
 
 # Define array params
-shape = [10000, 2000]
-pshape = [1000, 200]
+# shape = [10000, 2000]
+# pshape = [1000, 200]
+shape = [10 * 1000 * 1000]
+pshape = [200 * 1000]
+dtype = np.float64
 
 # Create initial containers
-a1 = ia.linspace(ia.dtshape(shape, pshape, np.float64), 0, 10)
-a2 = ia.iarray2numpy(a1)
+a1 = ia.linspace(ia.dtshape(shape, pshape, dtype), 0, 10)
+#a2 = ia.iarray2numpy(a1)
+a2 = np.linspace(0, 10, shape[0], dtype=dtype).reshape(shape)
+
 
 print("iarray evaluation...")
 
 # And now, the expression
-expr = ia.Expr(eval_flags="iterblock", blocksize=0)
+expr = ia.Expr(eval_flags="iterblosc", nthreads=1)
 expr.bind("x", a1)
 expr.compile("(x - 1.35) * (x - 4.45) * (x - 8.5)")
-b2 = expr.eval(shape, pshape, np.float64)
-
-b2_n = ia.iarray2numpy(b2)
+for i in range(NITER):
+    b1 = expr.eval(shape, pshape, np.float64)
+b1_n = ia.iarray2numpy(b1)
+print(b1_n)
 
 print("numpy evaluation...")
-b2 = eval("(x - 1.35) * (x - 4.45) * (x - 8.5)", {"x": a2})
+for i in range(NITER):
+    b2 = eval("(x - 1.35) * (x - 4.45) * (x - 8.5)", {"x": a2})
+print(b2)
 
 try:
-    np.testing.assert_almost_equal(b2, b2_n)
+    np.testing.assert_almost_equal(b2, b1_n)
     print("OK.  Results are the same.")
 except AssertionError:
     print("ERROR. Results are different.")
