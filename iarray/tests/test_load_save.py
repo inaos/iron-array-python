4f0364974e730e046907c8f3d26c985409faca93
Francesc Alted <faltet@gmail.com>
2020-04-03 13:55:23 +0200

    Disable MacOSX in azure and uncomment out the failing tests

--- iarray/tests/test_load_save.py
+++ iarray/tests/test_load_save.py
@@ -7,9 +7,9 @@ import numpy as np
 @pytest.mark.parametrize("shape, pshape, dtype, load_in_mem",
                          [
                              ([67], [20], np.float64, True),
-                             #([100, 123], [12, 21], np.float32, True),
-                             #([100, 100], [5, 17], np.float64, False),
-                             #([104, 121, 212], [5, 46, 10], np.float32, False)
+                             ([100, 123], [12, 21], np.float32, True),
+                             ([100, 100], [5, 17], np.float64, False),
+                             ([104, 121, 212], [5, 46, 10], np.float32, False)
 
                          ])
 def test_load_save(shape, pshape, dtype, load_in_mem):
