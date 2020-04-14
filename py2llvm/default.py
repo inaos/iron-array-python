ca2b23b8eaa7e21f3a83ff0806091f7d3c0cf3c1
J. David Ibáñez <jdavid.ibp@gmail.com>
2020-04-14 17:29:27 +0200

    Copy py2llvm

---
+++ py2llvm/default.py
@@ -0,0 +1,22 @@
+import ctypes
+
+
+def expand_argument(py_arg, c_type):
+    # Scalar
+    if isinstance(py_arg, (int, float)):
+        return [py_arg]
+
+    # Numpy
+    array_interface = getattr(py_arg, '__array_interface__', None)
+    if array_interface is not None:
+        array_interface = py_arg.__array_interface__
+        data = array_interface['data'][0]
+        shape = array_interface['shape']
+        return [data] + [n for n in shape]
+
+    # Struct
+    if isinstance(py_arg, ctypes.Structure):
+        address = ctypes.addressof(py_arg)
+        return [address]
+
+    return None
