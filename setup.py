8d796339f95378849d15afd150ad5575ee00815e
Francesc Alted <faltet@gmail.com>
2020-05-22 20:29:23 +0200

    Revert to Linux wheel builds

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from sys import platform
 from skbuild import setup
 
 DESCRIPTION = 'The Math Array Accelerator for Python'
@@ -5,18 +6,24 @@ DESCRIPTION = 'The Math Array Accelerator for Python'
 with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
 
+# Libraries to copy as 'data' in package
+# Copying just 'libarray' seems good enough
+if platform == "linux" or platform == "linux2":
+    copy_libs = ['libiarray.so']
+elif platform == "darwin":
+    copy_libs = ['libiarray.dylib']
+elif platform == "win32":
+    copy_lib = ['iarray.dll']  # TODO: not sure about this
+
 setup(
     name="iarray",
     description=DESCRIPTION,
     # long_description=LONG_DESCRIPTION,
     python_requires=">=3.6",
-    #package_dir={'': '.'},
     package_dir={'iarray': 'iarray'},
     packages=['iarray', 'iarray.py2llvm', 'iarray.tests'],
     package_data={
-        #'iarray': ['libsvml.so', 'libintlc.so', 'libintlc.so.5', 'libiarray.so'],
-        #'iarray': ['libintlc.so.5', 'libiarray.so'],  # for 'repairing' wheels
-        'iarray': ['libiarray.so'],  # leads to less heavier wheels (i.e. not 'repaired')
+        'iarray': copy_libs,
     },
     extras_require={
         'doc': [
