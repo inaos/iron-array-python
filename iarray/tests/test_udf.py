edf6baef1892774d0c7474f33887fcaa77258140
J. David Ibáñez <jdavid.ibp@gmail.com>
2020-06-02 11:25:31 +0200

    UDF tests: return type inferred from return statement
    Don't need to specify it.

--- iarray/tests/test_udf.py
+++ iarray/tests/test_udf.py
@@ -6,7 +6,7 @@ import pytest
 
 import iarray as ia
 from iarray import udf
-from iarray.py2llvm import float64, int64
+from iarray.py2llvm import float64
 
 
 def cmp_udf_np(f, start, stop, shape, pshape, dtype, cparams):
@@ -55,7 +55,7 @@ def cmp_udf_np_strict(f, start, stop, shape, pshape, dtype, cparams):
 
 
 @udf.jit
-def f_1dim(out: udf.Array(float64, 1), x: udf.Array(float64, 1)) -> int64:
+def f_1dim(out: udf.Array(float64, 1), x: udf.Array(float64, 1)):
     n = out.shape[0]
     for i in range(n):
         if i % 4 == 0:
@@ -80,7 +80,7 @@ def test_1dim(f):
 
 
 @udf.jit
-def f_2dim(out: udf.Array(float64, 2), x: udf.Array(float64, 2)) -> int64:
+def f_2dim(out: udf.Array(float64, 2), x: udf.Array(float64, 2)):
     n = x.shape[0]
     m = x.shape[1]
     for i in range(n):
@@ -103,7 +103,7 @@ def test_2dim(f):
 
 
 @udf.jit
-def f_avg(out: udf.Array(float64, 1), x: udf.Array(float64, 1)) -> int64:
+def f_avg(out: udf.Array(float64, 1), x: udf.Array(float64, 1)):
     n = x.shape[0]
     for i in range(n):
         value = x[i]
@@ -132,7 +132,7 @@ def test_avg(f):
 
 
 @udf.jit
-def f_error_bug(out: udf.Array(float64, 1), x: udf.Array(float64, 1)) -> int64:
+def f_error_bug(out: udf.Array(float64, 1), x: udf.Array(float64, 1)):
     n = out.shape[1]
     for i in range(n):
         out[i] = (math.sin(x[i]) - 1.35) * (x[i] - 4.45) * (x[i] - 8.5)
@@ -140,7 +140,7 @@ def f_error_bug(out: udf.Array(float64, 1), x: udf.Array(float64, 1)) -> int64:
     return 0
 
 @udf.jit
-def f_error_user(out: udf.Array(float64, 1), x: udf.Array(float64, 1)) -> int64:
+def f_error_user(out: udf.Array(float64, 1), x: udf.Array(float64, 1)):
     return 1
 
 @pytest.mark.skip('The test pass, but there is a segfault on finalization')
