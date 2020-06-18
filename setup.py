c50aa1e9fe184eb618e291e8e45b688bdf10648a
Francesc Alted <faltet@gmail.com>
2020-06-18 21:01:24 +0200

    New BUILD_WHEELS mode based on a environment variable

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
+import os
 from sys import platform
 from skbuild import setup
 
+BUILD_WHEELS = True if 'BUILD_WHEELS' in os.environ else False
+
 DESCRIPTION = 'The Math Array Accelerator for Python'
+
 
 with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
@@ -13,18 +17,26 @@ if platform == "linux" or platform == "linux2":
 elif platform == "darwin":
     copy_libs = ['libiarray.dylib']
 elif platform == "win32":
-    copy_lib = ['iarray.dll']  # TODO: not sure about this
+    copy_libs = ['iarray.dll']  # TODO: not sure about this
 
+if BUILD_WHEELS:
+    print("BUILD_WHEELS mode is ON!")
+    package_info = dict(
+        package_dir={'iarray': 'iarray'},
+        packages=['iarray', 'iarray.py2llvm', 'iarray.tests'],
+        package_data={'iarray': copy_libs},
+    )
+else:
+    # For some reason this is necessary for inplace compilation
+    # One can avoid using this if we nuke _skbuild/ next to iarray/
+    package_info = dict(
+        package_dir={'': '.'},
+    )
 setup(
     name="iarray",
     description=DESCRIPTION,
     # long_description=LONG_DESCRIPTION,
     python_requires=">=3.6",
-    package_dir={'iarray': 'iarray'},
-    packages=['iarray', 'iarray.py2llvm', 'iarray.tests'],
-    package_data={
-        'iarray': copy_libs,
-    },
     extras_require={
         'doc': [
             'sphinx >= 1.5',
@@ -36,4 +48,5 @@ setup(
             'numexpr',
             'numba',
         ]},
+    **package_info,
 )
