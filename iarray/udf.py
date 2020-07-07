ffd34fa3950d8671d135a04a03158e8a8dc193ee
J. David Ibáñez <jdavid.ibp@gmail.com>
2020-07-07 10:57:52 +0200

    Comment IR code for bound checking on window_shape
    Because it somehow conflicts with conditional expressions, producing
    this error:
    
    RuntimeError: PHI node entries do not match predecessors!

--- iarray/udf.py
+++ iarray/udf.py
@@ -8,7 +8,7 @@ from . import py2llvm
 from .py2llvm import int8, int8p, int32, int64, int32p, int64p
 from .py2llvm import types
 
-from . import iarray_ext
+#from . import iarray_ext
 
 assert math  # Silence pyflakes warning
 
@@ -44,12 +44,12 @@ class ArrayShape(types.ArrayShape):
         n_ir = types.value_to_ir_value(builder, n, type_=int8)
 
         # Check bounds
-        ndim = self.array.function._ndim
-        test = builder.icmp_signed('>=', n_ir, ndim)
-        with builder.if_then(test, likely=False):
-            return_type = builder.function.type.pointee.return_type
-            error = ir.Constant(return_type, iarray_ext.IARRAY_ERR_EVAL_ENGINE_OUT_OF_RANGE)
-            builder.ret(error)
+#       ndim = self.array.function._ndim
+#       test = builder.icmp_signed('>=', n_ir, ndim)
+#       with builder.if_then(test, likely=False):
+#           return_type = builder.function.type.pointee.return_type
+#           error = ir.Constant(return_type, iarray_ext.IARRAY_ERR_EVAL_ENGINE_OUT_OF_RANGE)
+#           builder.ret(error)
 
         # General case
         name = f'{self.name}_{n}'
