d894a6c69ea76b66350234937811b8d72841d13e
Francesc Alted <faltet@gmail.com>
2020-07-07 14:06:41 +0200

    Make the example expression a bit more complex

--- examples/expression_eval.py
+++ examples/expression_eval.py
@@ -11,14 +11,12 @@ pshape = [1000, 800]
 bshape = [100, 100]
 nthreads = 8
 
-sexpr = "cos(%s) + sin(%s)"
-npexpr = "np.cos(%s) + np.sin(%s)"
-# sexpr = "(%s - 1.35) * (%s - 4.45)"
-# npexpr = "(%s - 1.35) * (%s - 4.45)"
+sexpr = "(cos(%s) - sin(%s)) * (%s - 1.35) * (%s - 4.45)"
+npexpr = "(np.cos(%s) - np.sin(%s)) * (%s - 1.35) * (%s - 4.45)"
 
 # Create initial containers
 storage = ia.StorageProperties(backend="blosc", chunkshape=pshape, blockshape=bshape)
-kwargs = dict(storage=storage, nthreads=nthreads, clevel=9, clib=ia.LZ4, fp_mantissa_bits=30)
+kwargs = dict(storage=storage, nthreads=nthreads, clevel=9, clib=ia.LZ4, fp_mantissa_bits=24)
 
 size = shape[0] * shape[1]
 np0 = np.linspace(0, 10, size, dtype=dtype).reshape(shape)
@@ -27,13 +25,13 @@ np1 = np.linspace(0, 1, size, dtype=dtype).reshape(shape)
 ia1 = ia.numpy2iarray(np1, **kwargs)
 
 t0 = time()
-np2 = eval(npexpr % ("np0", "np1"))
+np2 = eval(npexpr % (("np0", "np1") * 2))
 t1 = time()
 print("Time for numpy evaluation: %.3f" % (t1 - t0))
 
 ne.set_num_threads(nthreads)
 t0 = time()
-np3 = ne.evaluate(sexpr % ("np0", "np1"))
+np3 = ne.evaluate(sexpr % (("np0", "np1") * 2))
 t1 = time()
 print("Time for numexpr evaluation: %.3f" % (t1 - t0))
 
@@ -49,14 +47,14 @@ expr = ia.Expr(eval_flags=eval_flags, **kwargs)
 expr.bind("x", ia0)
 expr.bind("y", ia1)
 expr.bind_out_properties(ia.dtshape(shape, dtype), storage=storage)
-expr.compile(sexpr % ("x", "y"))
+expr.compile(sexpr % (("x", "y") * 2))
 ia2 = expr.eval()
 t1 = time()
 print("Time for iarray evaluation: %.3f (cratio: %.2fx)" % ((t1 - t0), ia2.cratio))
 np3 = ia.iarray2numpy(ia2)
 
 try:
-    np.testing.assert_almost_equal(np3, np2)
+    np.testing.assert_almost_equal(np3, np2, decimal=5)
     print("OK.  Results are the same.")
 except AssertionError:
     print("ERROR. Results are different.")
