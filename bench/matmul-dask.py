b246e43070a3165e403731a2eb52ed6dd25d3938
Aleix Alcacer Sales <aleixalcacer@gmail.com>
2020-10-16 09:36:57 +0200

    Improve profilings

--- bench/matmul-dask.py
+++ bench/matmul-dask.py
@@ -4,7 +4,8 @@ import zarr
 import dask.array as da
 import dask
 import sys
-
+import os
+import shutil
 import ctypes
 
 mkl_rt = ctypes.CDLL("libmkl_rt.dylib")
@@ -16,6 +17,9 @@ persistent = True
 shape = (8192, 8192)
 chunkshape = (2048, 2048)
 
+shape = (1000, 1000)
+chunkshape = (500, 500)
+
 if len(sys.argv) == 1:
     nthreads = 1
 else:
@@ -25,26 +29,14 @@ cname = "lz4"
 clevel = 5
 shuffle = Blosc.SHUFFLE
 
-if persistent:
-    import os
-    import shutil
-
-    afilename = "a.zarr"
-    if os.path.exists(afilename):
-        shutil.rmtree(afilename)
-    bfilename = "b.zarr"
-    if os.path.exists(bfilename):
-        shutil.rmtree(bfilename)
-    cfilename = "c.zarr"
-    if os.path.exists(cfilename):
-        shutil.rmtree(cfilename)
+afilename = "a.zarr"
+bfilename = "b.zarr"
+cfilename = "c.zarr"
 
 compressor = Blosc(cname=cname, clevel=clevel, shuffle=shuffle)
 
-
-@profile
-def create_data():
-    if persistent:
+if persistent:
+    if not os.path.exists(afilename):
         azarr = zarr.open(
             afilename,
             mode="w",
@@ -53,6 +45,11 @@ def create_data():
             dtype=np.float64,
             compressor=compressor,
         )
+        tmp = np.linspace(-1, 1, int(np.prod(shape))).reshape(shape)
+        azarr[:] = tmp
+    else:
+        azarr = zarr.load("a.zarr")
+    if not os.path.exists(bfilename):
         bzarr = zarr.open(
             bfilename,
             mode="w",
@@ -61,55 +58,39 @@ def create_data():
             dtype=np.float64,
             compressor=compressor,
         )
+        tmp = np.linspace(-1, 1, int(np.prod(shape))).reshape(shape)
+        bzarr[:] = tmp
     else:
-        azarr = zarr.empty(shape=shape, chunks=chunkshape, dtype=np.float64, compressor=compressor)
-        bzarr = zarr.empty(shape=shape, chunks=chunkshape, dtype=np.float64, compressor=compressor)
-
-    tmp = np.linspace(-1, 1, int(np.prod(shape))).reshape(shape)
-    azarr[:] = tmp
-    bzarr[:] = tmp
-    del tmp
-    return azarr, bzarr
+        bzarr = zarr.load("b.zarr")
+else:
+    azarr = zarr.empty(shape=shape, chunks=chunkshape, dtype=np.float64, compressor=compressor)
+    bzarr = zarr.empty(shape=shape, chunks=chunkshape, dtype=np.float64, compressor=compressor)
 
 
-azarr, bzarr = create_data()
+scheduler = "single-threaded" if nthreads == 1 else "threads"
+with dask.config.set(scheduler=scheduler, num_workers=nthreads):
+    if persistent:
+        czarr = zarr.open(
+            cfilename,
+            mode="w",
+            shape=shape,
+            chunks=chunkshape,
+            dtype=np.float64,
+            compressor=compressor,
+        )
+    else:
+        czarr = zarr.empty(shape=shape, chunks=chunkshape, dtype=np.float64, compressor=compressor)
 
+    adask = da.from_zarr(azarr)
+    bdask = da.from_zarr(bzarr)
+    cdask = da.matmul(adask, bdask)
+    da.to_zarr(cdask, czarr)
 
-@profile
-def matmul():
-    scheduler = "single-threaded" if nthreads == 1 else "threads"
-    with dask.config.set(scheduler=scheduler, num_workers=nthreads):
-        if persistent:
-            czarr = zarr.open(
-                cfilename,
-                mode="w",
-                shape=shape,
-                chunks=chunkshape,
-                dtype=np.float64,
-                compressor=compressor,
-            )
-        else:
-            czarr = zarr.empty(
-                shape=shape, chunks=chunkshape, dtype=np.float64, compressor=compressor
-            )
-
-        adask = da.from_zarr(azarr)
-        bdask = da.from_zarr(bzarr)
-        cdask = da.matmul(adask, bdask)
-        da.to_zarr(cdask, czarr)
-        return czarr
-
-
-czarr = matmul()
-
-print(azarr.info)
-print(bzarr.info)
-print(czarr.info)
 
 if persistent:
-    if os.path.exists(afilename):
-        shutil.rmtree(afilename)
-    if os.path.exists(bfilename):
-        shutil.rmtree(bfilename)
+    # if os.path.exists(afilename):
+    #     shutil.rmtree(afilename)
+    # if os.path.exists(bfilename):
+    #     shutil.rmtree(bfilename)
     if os.path.exists(cfilename):
         shutil.rmtree(cfilename)
--- bench/matmul-iarray.py
+++ bench/matmul-iarray.py
@@ -1,12 +1,17 @@
 import iarray as ia
 import numpy as np
 import sys
+import os
 
-persistent = False
+persistent = True
 
 shape = (8192, 8192)
 chunkshape = (2048, 2048)
 blockshape = (512, 512)
+
+shape = (1000, 1000)
+chunkshape = (500, 500)
+blockshape = (250, 250)
 
 if len(sys.argv) == 1:
     nthreads = 1
@@ -17,17 +22,9 @@ clib = ia.LZ4
 clevel = 5
 
 if persistent:
-    import os
-
     afilename = "a.iarray"
-    if os.path.exists(afilename):
-        os.remove(afilename)
     bfilename = "b.iarray"
-    if os.path.exists(bfilename):
-        os.remove(bfilename)
     cfilename = "c.iarray"
-    if os.path.exists(cfilename):
-        os.remove(cfilename)
 else:
     afilename = None
     bfilename = None
@@ -41,34 +38,29 @@ cstorage = ia.StorageProperties(chunkshape, blockshape, filename=cfilename)
 
 cparams = dict(clib=clib, clevel=clevel, nthreads=nthreads)
 
-
-@profile
-def create_data():
+if persistent:
+    if not os.path.exists(afilename):
+        aia = ia.linspace(dtshape, -1, 1, storage=astorage, **cparams)
+    else:
+        aia = ia.load("a.iarray", load_in_mem=False)
+else:
     aia = ia.linspace(dtshape, -1, 1, storage=astorage, **cparams)
-    bia = ia.linspace(dtshape, -1, 1, storage=bstorage, **cparams)
-    return aia, bia
-
-
-aia, bia = create_data()
-
-
-@profile
-def matmul():
-    cia = ia.matmul(aia, bia, storage=cstorage, **cparams)
-    return cia
-
-
-cia = matmul()
-
 
 if persistent:
-    if os.path.exists(afilename):
-        os.remove(afilename)
-    if os.path.exists(bfilename):
-        os.remove(bfilename)
+    if not os.path.exists(bfilename):
+        bia = ia.linspace(dtshape, -1, 1, storage=bstorage, **cparams)
+    else:
+        bia = ia.load("b.iarray", load_in_mem=False)
+else:
+    bia = ia.linspace(dtshape, -1, 1, storage=bstorage, **cparams)
+
+print("start matmul")
+cia = ia.matmul(aia, bia, storage=cstorage, **cparams)
+
+if persistent:
+    # if os.path.exists(afilename):
+    #     os.remove(afilename)
+    # if os.path.exists(bfilename):
+    #     os.remove(bfilename)
     if os.path.exists(cfilename):
         os.remove(cfilename)
-
-print(aia.cratio)
-print(bia.cratio)
-print(cia.cratio)
