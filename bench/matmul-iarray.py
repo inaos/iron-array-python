2c5fe7bc54ca169966b1b9e9215b7d57ed943dcf
Francesc Alted <faltet@gmail.com>
2020-11-02 11:53:36 +0100

    Check that sizes have not changed when in persistent mode

--- bench/matmul-iarray.py
+++ bench/matmul-iarray.py
@@ -2,6 +2,7 @@ import iarray as ia
 import numpy as np
 import sys
 import os
+from time import time
 
 persistent = True
 
@@ -9,8 +10,8 @@ persistent = True
 # chunkshape = (4096, 4096)
 # blockshape = (1024, 1024)
 
-shape = (500, 500)
-chunkshape = (250, 250)
+shape = (1000, 1000)
+chunkshape = (500, 500)
 blockshape = (100, 100)
 
 if len(sys.argv) == 1:
@@ -37,25 +38,50 @@ bstorage = ia.Storage(chunkshape, blockshape, filename=bfilename)
 cstorage = ia.Storage(chunkshape, blockshape, filename=cfilename)
 
 ia.set_config(codec=codec, clevel=clevel, nthreads=nthreads)
+cfg = ia.get_config()
 
+print("(Re-)Generating operand A")
 if persistent:
     if not os.path.exists(afilename):
         aia = ia.linspace(dtshape, -1, 1, storage=astorage)
     else:
         aia = ia.load("a.iarray", load_in_mem=False)
+        if (
+            aia.dtshape.shape != shape
+            or aia.chunkshape != chunkshape
+            or aia.blockshape != blockshape
+        ):
+            # Ooops, we cannot use the array on-disk.  Regenerate it.
+            os.remove(afilename)
+            aia = ia.linspace(dtshape, -1, 1, storage=astorage)
 else:
     aia = ia.linspace(dtshape, -1, 1, storage=astorage)
 
+print("(Re-)Generating operand B")
 if persistent:
     if not os.path.exists(bfilename):
         bia = ia.linspace(dtshape, -1, 1, storage=bstorage)
     else:
         bia = ia.load("b.iarray", load_in_mem=False)
+        if (
+            bia.dtshape.shape != shape
+            or bia.chunkshape != chunkshape
+            or bia.blockshape != blockshape
+        ):
+            # Ooops, we cannot use the array on-disk.  Regenerate it.
+            os.remove(bfilename)
+            bia = ia.linspace(dtshape, -1, 1, storage=bstorage)
 else:
     bia = ia.linspace(dtshape, -1, 1, storage=bstorage)
 
-print(f"start matmul with nthreads={nthreads}")
+if persistent:
+    if os.path.exists(cfilename):
+        os.remove(cfilename)
+
+print(f"Start actual matmul with nthreads = {cfg.nthreads}")
+t0 = time()
 cia = ia.matmul(aia, bia, storage=cstorage)
+print("Time for iarray matmul:", round((time() - t0), 3))
 
 if persistent:
     # if os.path.exists(afilename):
