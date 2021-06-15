f59e4576633f4c367c3957d43f3c224d86e8c53c
FrancescAlted <francesc@blosc.org>
2021-06-15 14:16:56 +0200

    Go back to favor=BALANCE as default

--- iarray/config_params.py
+++ iarray/config_params.py
@@ -129,7 +129,7 @@ class Defaults(object):
     _config = None
     codec: ia.Codecs = ia.Codecs.LZ4
     clevel: int = 9
-    favor: ia.Favors = ia.Favors.SPEED
+    favor: ia.Favors = ia.Favors.BALANCE
     use_dict: bool = False
     filters: List[ia.Filters] = field(default_factory=default_filters)
     nthreads: int = 0
