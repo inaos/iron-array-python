1701604a3a5e16564191d4f98eb75598a52996e4
Aleix Alcacer <aleixalcacer@gmail.com>
2021-08-04 09:25:20 +0200

    Fix issues

--- examples/gemv.py
+++ examples/gemv.py
@@ -4,29 +4,23 @@ from time import time
 import numpy as np
 
 
-_ = ia.set_config(clevel=9, codec=ia.Codecs.LZ4, filters=[], btune=False, nthreads=8)
+_ = ia.set_config(clevel=1, codec=ia.Codecs.LZ4, btune=False, nthreads=8, dtype=np.float64, seed=0)
 
-# w = scipy.io.mmread("/Users/aleix11alcacer/Downloads/worms20_10NN/worms20_10NN.mtx")
-# wnp = w.toarray()
+shape = (25000, 13859)
+# wia = ia.random.normal(shape, 3, 2, chunks=(1024, 1024), blocks=(128, 128), enforce_frame=True, urlpath="dense.iarray", fp_mantissa_bits=3)
 
-#
-# wia = ia.numpy2iarray(wnp, chunks=(1 * 1024, 1 * 1024), blocks=(64, 64), enforce_frame=True, urlpath="gemv.iarray", mode="w")
-wia = ia.load("gemv.iarray")
+wia = ia.load("dense.iarray")
 print(wia.info)
-
-# shape = (8 * 2048, 8 * 2048)
-# wia = ia.linspace(shape, 0, 1, chunks=(1024, 1024), blocks=(128, 128))
-# wia[:250, :] = 0
 
 w = wia.data
 
-bia = ia.linspace((wia.shape[1],), 0, 1, chunks=(wia.chunks[1],), blocks=(wia.blocks[1],))
+bia = ia.linspace((wia.shape[1],), 2, 10, chunks=(wia.chunks[1],), blocks=(wia.blocks[1],))
 
 # b[256:512] = 0
 t0 = time()
 c = w.dot(bia.data)
 t1 = time()
-print(f"scipy: {t1 - t0:.5f} s")
+print(f"numpy: {t1 - t0:.5f} s")
 
 t0 = time()
 cia = ia.gemv(wia, bia, chunks=(wia.chunks[0],), blocks=(wia.blocks[0],))
@@ -40,4 +34,5 @@ t1 = time()
 
 print(f"iarray: {t1 - t0:.5f} s")
 
-np.testing.assert_allclose(cia.data, c, rtol=1e-6)
+
+np.testing.assert_allclose(cia.data, c, rtol=1e-10 if w.dtype == np.float64 else 1e-4)
