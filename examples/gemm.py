8090b5aaa88f2ce901ef329705793ce78663507e
Aleix Alcacer <aleixalcacer@gmail.com>
2021-09-23 09:22:21 +0200

    Improve opt_gemm2_params

--- examples/gemm.py
+++ examples/gemm.py
@@ -2,23 +2,26 @@ import iarray as ia
 from time import time
 import numpy as np
 
-M = 1_000
-K = 5_000
-N = 25_000
+M = 1
+K = 3
+N = 15_000
 
 dtype = np.dtype(np.float32)
 
-params = ia.opt_gemm2_params(M, K, N, itemsize=dtype.itemsize, l2_size=512 * 1024)
+params = ia.opt_gemm2_params(M, K, N, itemsize=dtype.itemsize, l2_size=256 * 1024)
 print(params)
-# t = np.ones((M, K), dtype=np.float64)
-# t = np.tril(t)
-# a = ia.numpy2iarray(t, chunks=params["a_chunks"], blocks=params["a_blocks"])
-a = ia.random.random_sample(
-    (M, K), dtype=dtype, chunks=params["a_chunks"], blocks=params["a_blocks"]
-)
+t = np.ones((M, K), dtype=dtype)
+t = np.tril(t)
+a = ia.numpy2iarray(t, chunks=params["a_chunks"], blocks=params["a_blocks"])
+# a = ia.random.random_sample(
+#     (M, K), dtype=dtype, chunks=params["a_chunks"], blocks=params["a_blocks"]
+# )
 
 b = ia.random.random_sample(
-    (K, N), dtype=dtype, chunks=params["b_chunks"], blocks=params["b_blocks"]
+    (K, N),
+    dtype=dtype,
+    chunks=params["b_chunks"],
+    blocks=params["b_blocks"],
 )
 
 t0 = time()
@@ -33,4 +36,4 @@ c_np = a_np @ b_np
 t1 = time()
 print(f"Time numpy: {t1 - t0:.4f} s")
 
-np.testing.assert_allclose(c2.data, c_np, rtol=1e-6)
+np.testing.assert_allclose(c2.data, c_np, rtol=1e-5)
