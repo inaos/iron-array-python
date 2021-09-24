f933b0528e59eacd8d9282fd3c8c96b5ec139f4f
Aleix Alcacer <aleixalcacer@gmail.com>
2021-09-24 13:09:12 +0200

    Merge all gemm algorithms

--- examples/gemm.py
+++ examples/gemm.py
@@ -2,36 +2,42 @@ import iarray as ia
 from time import time
 import numpy as np
 
-M = 634
-K = 26_232
-N = 25_000
+M = 250_000
+K = 5_000
+N = 1_000
 
-algorithm = "gemm2"
-nreps = 3
+algorithm = "gemm3"
+nreps = 1
 
 dtype = np.dtype(np.float32)
 
-params = getattr(ia, f"opt_{algorithm}_params")(
-    M, K, N, itemsize=dtype.itemsize, l2_size=512 * 1024
-)
+params = ia.opt_gemm_params(M, K, N, itemsize=dtype.itemsize, l2_size=512 * 1024)
 print(params)
-# t = np.ones((M, K), dtype=dtype)
-# # t = np.tril(t)
-# a = ia.numpy2iarray(t, chunks=params["a_chunks"], blocks=params["a_blocks"])
-a = ia.random.random_sample(
-    (M, K), dtype=dtype, chunks=params["a_chunks"], blocks=params["a_blocks"]
-)
+a_chunks = params["a_chunks"]
+a_blocks = params["a_blocks"]
+b_chunks = params["b_chunks"]
+b_blocks = params["b_blocks"]
+
+a_chunks = (1000, 1000)
+b_chunks = (1000, 1000)
+a_blocks = (200, 200)
+b_blocks = (200, 200)
+c_chunks = (1000, 1000)
+c_blocks = (200, 200)
+
+a = ia.random.random_sample((M, K), dtype=dtype, chunks=a_chunks, blocks=b_blocks)
 
 b = ia.random.random_sample(
     (K, N),
     dtype=dtype,
-    chunks=params["b_chunks"],
-    blocks=params["b_blocks"],
+    chunks=b_chunks,
+    blocks=b_blocks,
 )
+
 
 t0 = time()
 for i in range(nreps):
-    c2 = getattr(ia, f"opt_{algorithm}")(a, b)
+    c2 = ia.opt_gemm(a, b, ott_b=True, chunks=c_chunks, blocks=c_blocks)
 t1 = time()
 print(f"Time iarray: {(t1 - t0) / nreps:.4f} s")
 
