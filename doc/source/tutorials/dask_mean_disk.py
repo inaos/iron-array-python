e56b28551d13571ef724e75e7b1e7f87580522c7
Marta Iborra <martaiborra24@gmail.com>
2021-10-29 09:26:49 +0200

    improve and rerun tutorials 3, 5, 6, 7, 8 and 10

--- doc/source/tutorials/dask_mean_disk.py
+++ doc/source/tutorials/dask_mean_disk.py
@@ -4,7 +4,10 @@ import iarray as ia
 import dask
 import dask.array as da
 import zarr
+import os
 
+cmd = 'vmtouch -e precip-3m.zarr'
+os.system(cmd)
 # Use a previous computation for getting metadata (shape, chunks...)
 precip = zarr.open("precip-3m.zarr")
 shape = precip.shape[1:]
@@ -16,10 +19,13 @@ compressor = zarr.Blosc(clevel=1, cname="zstd", shuffle=zarr.Blosc.BITSHUFFLE)
 #zarr.save("precip2.zarr", precip[1])
 #zarr.save("precip3.zarr", precip[2])
 
+cmd2 = 'vmtouch -e precip1.zarr precip2.zarr precip3.zarr'
+os.system(cmd2)
 zprecip1 = zarr.open("precip1.zarr")
 zprecip2 = zarr.open("precip2.zarr")
 zprecip3 = zarr.open("precip3.zarr")
 
+os.system(cmd2)
 precip1 = da.from_zarr(zprecip1)
 precip2 = da.from_zarr(zprecip2)
 precip3 = da.from_zarr(zprecip3)
@@ -38,7 +44,9 @@ def dask_mean_disk(expr):
         da.to_zarr(expr, expr_val)
     return expr_val
 
+os.system(cmd2)
 mean_expr = (precip1 + precip2 + precip3) / 3
+os.system(cmd2)
 mean_disk = dask_mean_disk(mean_expr)
 
 @profile
@@ -54,5 +62,7 @@ def dask_trans_disk(expr):
         da.to_zarr(expr, expr_val)
     return expr_val
 
+os.system(cmd2)
 trans_expr = np.tan(precip1) * (np.sin(precip1) * np.sin(precip2) + np.cos(precip2)) + np.sqrt(precip3) * 2
+os.system(cmd2)
 trans_disk = dask_trans_disk(trans_expr)
