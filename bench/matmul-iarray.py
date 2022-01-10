922a49e969e40dd2a294539e59aa3fd729b54964
Francesc Alted <francesc@blosc.org>
2022-01-10 10:16:41 +0100

    Update bench to latest API

--- bench/matmul-iarray.py
+++ bench/matmul-iarray.py
@@ -1,26 +1,9 @@
 import iarray as ia
-import numpy as np
-import sys
 import os
 from time import time
 
 persistent = True
-
-# shape = (16384, 16384)
-# chunks = (4096, 4096)
-# blocks = (1024, 1024)
-
-shape = (1000, 1000)
-chunks = (500, 500)
-blocks = (100, 100)
-
-if len(sys.argv) == 1:
-    nthreads = 8
-else:
-    nthreads = int(sys.argv[1])
-
-codec = ia.Codec.LZ4
-clevel = 5
+shape = (10_000, 10_000)
 
 if persistent:
     aurlpath = "a.iarr"
@@ -31,52 +14,49 @@ else:
     burlpath = None
     curlpath = None
 
-acfg = ia.Config(chunks=chunks, blocks=blocks, urlpath=aurlpath)
-bcfg = ia.Config(chunks=chunks, blocks=blocks, urlpath=burlpath)
-ccfg = ia.Config(chunks=chunks, blocks=blocks, urlpath=curlpath)
-
-ia.set_config_defaults(codec=codec, clevel=clevel, nthreads=nthreads, dtype=np.float64, btune=False)
-cfg = ia.get_config_defaults()
+# Obtain optimal chunk and block shapes
+mparams = ia.matmul_params(shape, shape)
+amchunks, amblocks, bmchunks, bmblocks = mparams
+print(mparams)
+#ia.set_config_defaults(chunks=amchunks, blocks=amblocks, favor=ia.Favor.SPEED)
+# For some reason, disabling BTune make better cratio and similar speed.
+ia.set_config_defaults(chunks=amchunks, blocks=amblocks, btune=False)
 
 print("(Re-)Generating operand A")
 if persistent:
     if not os.path.exists(aurlpath):
-        aia = ia.linspace(shape, -1, 1, cfg=acfg)
+        a = ia.linspace(shape, -1, 1, urlpath=aurlpath)
     else:
-        aia = ia.open("a.iarr")
-        if aia.shape != shape or aia.chunks != chunks or aia.blocks != blocks:
-            # Ooops, we cannot use the array on-disk.  Regenerate it.
-            ia.remove_urlpath(aurlpath)
-            aia = ia.linspace(shape, -1, 1, cfg=acfg)
+        a = ia.open("a.iarr")
 else:
-    aia = ia.linspace(shape, -1, 1, cfg=acfg)
+    a = ia.linspace(shape, -1, 1, urlpath=aurlpath)
 
 print("(Re-)Generating operand B")
 if persistent:
     if not os.path.exists(burlpath):
-        bia = ia.linspace(shape, -1, 1, cfg=bcfg)
+        b = ia.linspace(shape, -1, 1, urlpath=burlpath)
     else:
-        bia = ia.open("b.iarr")
-        if bia.shape != shape or bia.chunks != chunks or bia.blocks != blocks:
-            # Ooops, we cannot use the array on-disk.  Regenerate it.
-            ia.remove_urlpath(burlpath)
-            bia = ia.linspace(shape, -1, 1, cfg=bcfg)
+        b = ia.open("b.iarr")
 else:
-    bia = ia.linspace(shape, -1, 1, cfg=bcfg)
+    b = ia.linspace(shape, -1, 1, urlpath=burlpath)
 
 if persistent:
     if os.path.exists(curlpath):
         ia.remove_urlpath(curlpath)
 
+print(f"chunks a: {a.chunks}, b: {b.chunks}")
+print(f"cratio a: {a.cratio:.2f}, b: {b.cratio:.2f}")
+cfg = ia.get_config_defaults()
 print(f"Start actual matmul with nthreads = {cfg.nthreads}")
 t0 = time()
-cia = ia.matmul(aia, bia, cfg=ccfg)
+c = ia.matmul(a, b, urlpath=curlpath)
 print("Time for iarray matmul:", round((time() - t0), 3))
+print(f"cratio c: {c.cratio:.2f}")
 
 if persistent:
-    # if os.path.exists(aurlpath):
-    #     ia.remove_urlpath(aurlpath)
-    # if os.path.exists(burlpath):
-    #     ia.remove_urlpath(burlpath)
+    if os.path.exists(aurlpath):
+        ia.remove_urlpath(aurlpath)
+    if os.path.exists(burlpath):
+        ia.remove_urlpath(burlpath)
     if os.path.exists(curlpath):
         ia.remove_urlpath(curlpath)
