585997a8d1da54a81101d50846471c42561c137c
Aleix Alcacer <aleixalcacer@gmail.com>
2022-02-11 12:45:57 +0100

    Fix rebase conflicts

--- examples/reduce.py
+++ examples/reduce.py
@@ -5,14 +5,14 @@ import numpy as np
 import os
 
 ia.remove_urlpath("red.iarr")
-a = ia.arange([10, 20, 10, 14], clevel=9, btune=False)
+a = ia.arange([20, 20, 10, 14], clevel=9, btune=False)
 
-b = a[0]
+b = a[5:10]
 
-an = ia.iarray2numpy(a)
+an = ia.iarray2numpy(b)
 
 cn2 = np.mean(an, axis=(2, 3, 1))
-cn = ia.mean(a, axis=(2, 3, 1), chunks=(4,), blocks=(4,), urlpath="red.iarr")
+cn = ia.mean(b, axis=(2, 3, 1), chunks=(4,), blocks=(4,), urlpath="red.iarr")
 d = ia.open("red.iarr")
 
 np.testing.assert_allclose(d.data, cn2)
