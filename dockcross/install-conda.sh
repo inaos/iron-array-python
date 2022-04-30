9d2fd356d1bea75d9f0227c29dda34711e63f279
Francesc Alted <francesc@blosc.org>
2022-04-30 09:53:45 +0200

    Switch back to LLVM in numba channel

--- dockcross/install-conda.sh
+++ dockcross/install-conda.sh
@@ -35,9 +35,10 @@ conda update --all --yes
 conda clean -tipy
 
 # Install conda build and deployment tools.
-# conda install -y --quiet -c numba/label/manylinux2014 llvmdev
-# LLVM 13 is necessary for calling int intrinsics
-conda install -y -c conda-forge/label/cf202003 'llvmdev>=13'
+conda install -y --quiet -c numba/label/manylinux2014 llvmdev
+# LLVM 13 should be necessary for calling int intrinsics, but these do not work yet
+# conda install -y -c conda-forge 'llvmdev>=13'
+# conda install -y -c conda-forge/label/cf202003 'llvmdev>=13'
 conda install -y --quiet -c intel mkl-include
 conda install -y --quiet -c intel mkl-static
 conda install -y --quiet -c intel icc_rt
