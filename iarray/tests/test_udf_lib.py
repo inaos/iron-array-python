79aab3f3b86617bb9be2a2121e403f03d18b9367
J. David Ibáñez <jdavid.ibp@gmail.com>
2022-05-02 17:57:57 +0200

    UdfRegistry.libs_funcs values are now dicts
    This fixes the check whether the function is aleady registered or not.

--- iarray/tests/test_udf_lib.py
+++ iarray/tests/test_udf_lib.py
@@ -31,13 +31,13 @@ def fmult(a: udf.float64, b: udf.float64) -> float:
 )
 def test_simple(sexpr, sexpr_udf, inputs):
     assert "lib" in ia.udf_registry
-    assert {"fsum"} == set(f.name for f in ia.udf_registry["lib"])
+    assert {"fsum"} == set(fname for fname in ia.udf_registry["lib"])
     assert "fsum" in (f.name for f in ia.udf_registry.iter_funcs("lib"))
     assert "lib.fsum" in (f for f in ia.udf_registry.iter_all_func_names())
     assert ia.udf_registry.get_func_addr("lib.fsum") != 0  # this can be any integer
 
     assert "lib2" in ia.udf_registry
-    assert {"fmult"} == set(f.name for f in ia.udf_registry["lib2"])
+    assert {"fmult"} == set(fname for fname in ia.udf_registry["lib2"])
     assert "fmult" in (f.name for f in ia.udf_registry.iter_funcs("lib2"))
     assert "lib2.fmult" in (f for f in ia.udf_registry.iter_all_func_names())
     assert {"lib.fsum", "lib2.fmult"} == set(ia.udf_registry.iter_all_func_names())
