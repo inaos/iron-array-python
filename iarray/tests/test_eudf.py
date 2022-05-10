d7a0d3416bff2ba75707c0a8e332db8d5714552e
J. David Ibáñez <jdavid.ibp@gmail.com>
2022-05-10 19:40:02 +0200

    Expression to UDF: masks

--- iarray/tests/test_eudf.py
+++ iarray/tests/test_eudf.py
@@ -2,7 +2,13 @@ import numpy as np
 import pytest
 
 import iarray as ia
+from iarray import udf
 from iarray.eudf import eudf
+
+
+@udf.scalar(lib="lib")
+def fsum(a: udf.float64, b: udf.float64) -> float:
+    return a + b
 
 
 @pytest.mark.parametrize(
@@ -18,6 +24,7 @@ from iarray.eudf import eudf
         ("2 + x * x * (x + x)", {"x": ia.arange((10,))}),
         ("2 + x * x * ((x * x) + x)", {"x": ia.arange((10,))}),
         ("x * y * ((x * y) + y)", {"x": ia.arange((10,)), "y": 2}),
+        ("lib.fsum(x, x)", {"x": ia.arange((10,))}),
     ],
 )
 def test_simple(sexpr, inputs):
@@ -27,3 +34,21 @@ def test_simple(sexpr, inputs):
     out_udf = expr_udf.eval()
     tol = 1e-14
     np.testing.assert_allclose(out.data, out_udf.data, rtol=tol, atol=tol)
+
+
+@pytest.mark.parametrize(
+    "sexpr, sexpr_np, inputs",
+    [
+        ('a[b > 5.0]', 'b > 5.0', {'a': ia.arange([10]), 'b': ia.arange([10])}),
+    ]
+)
+def test_complex(sexpr, sexpr_np, inputs):
+    expr_udf = eudf(sexpr, inputs)
+    out_udf = expr_udf.eval()
+
+    a = inputs['a'].data
+    b = inputs['b'].data
+    np_out = np.where(eval(sexpr_np), a, 0)
+
+    tol = 1e-14
+    np.testing.assert_allclose(np_out, out_udf.data, rtol=tol, atol=tol)
