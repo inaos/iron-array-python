201dbde53af8d174f5a48b77ad68b1d41d6c6bec
Aleix Alcacer <aleixalcacer@gmail.com>
2022-07-01 09:44:07 +0200

    Fix red

--- examples/reduce.py
+++ examples/reduce.py
@@ -3,13 +3,42 @@
 import iarray as ia
 import numpy as np
 
+shape = [5, 5]
+chunks = [5, 5]
+blocks = [2, 2]
 
-a = ia.arange([20, 20, 20, 20], btune=False)
-print(a.info)
+axis = (0, 1)
 
+dtype = np.float64
+urlpath1 = "example_linspace.iarr"
+urlpath2 = "example_reduce.iarr"
+
+ia.remove_urlpath(urlpath1)
+ia.remove_urlpath(urlpath2)
+
+a = ia.arange(
+    shape, chunks=chunks, blocks=blocks, dtype=dtype, urlpath=urlpath1, btune=False, mode="w"
+)
+slices = tuple([slice(np.random.randint(1, s)) for s in a.shape])
+a[slices] = np.nan
+print(slices)
 a_data = a.data
-cn2 = np.std(a_data, axis=(3,))
-cn = ia.std(a, axis=3)
+cn2 = np.nanmean(a_data, axis=axis)
+cn = ia.nanmean(a, urlpath=urlpath2, axis=axis)
 
-np.testing.assert_allclose(cn.data, cn2)
+print(a_data)
+
+# np.testing.assert_allclose(cn.data, cn2)
 print("Matrix reduction is working!")
+
+ia.remove_urlpath(urlpath1)
+ia.remove_urlpath(urlpath2)
+
+# a = np.random.random_sample(np.prod(shape)).reshape(shape)
+# a[...] = np.nan
+
+# a1 = np.nanmean(np.nanmean(a, axis=1), axis=0)
+# a2 = np.nanmean(a, axis=(1, 0))
+# np.testing.assert_allclose(a1, a2)
+
+print(np.median(a_data, axis=0))
